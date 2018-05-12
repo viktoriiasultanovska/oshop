f34634e607fe48627e1990083d8b60be29d6e650
Victoria Sultanovska <viktoriia.sultanovska@orba.co>
2018-05-12 23:44:44 +0200

    19.285. Project: Deployment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,11 @@
 export const environment = {
   production: true,
-  apiKey: "AIzaSyDVH8anVoWv9A_YEjYSTy2TNW0MmXjoonI",
-  authDomain: "oshop-3eabc.firebaseapp.com",
-  databaseURL: "https://oshop-3eabc.firebaseio.com",
-  projectId: "oshop-3eabc",
-  storageBucket: "",
-  messagingSenderId: "1011573945239"
+  firebase: {
+    apiKey: "AIzaSyDVH8anVoWv9A_YEjYSTy2TNW0MmXjoonI",
+    authDomain: "oshop-3eabc.firebaseapp.com",
+    databaseURL: "https://oshop-3eabc.firebaseio.com",
+    projectId: "oshop-3eabc",
+    storageBucket: "",
+    messagingSenderId: "1011573945239"
+  }
 };
