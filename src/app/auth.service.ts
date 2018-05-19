265c7c2ed1530cc55740ee0541df393560413091
Victoria Sultanovska <viktoriia.sultanovska@orba.co>
2018-05-19 23:50:08 +0200

    20.293. Project: Authentication and Authorization Redirect After Logging In

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -2,17 +2,24 @@ import {Injectable} from '@angular/core';
 import {AngularFireAuth} from 'angularfire2/auth';
 import * as firebase from 'firebase';
 import {Observable} from 'rxjs/Observable';
+import {ActivatedRoute, Router} from '@angular/router';
 
 @Injectable()
 export class AuthService {
   user$: Observable<firebase.User>;
 
-  constructor(private afAuth: AngularFireAuth) {
+  constructor(private afAuth: AngularFireAuth, private route: ActivatedRoute, private router: Router) {
     this.user$ = afAuth.authState;
   }
 
   login() {
-    this.afAuth.auth.signInWithRedirect(new firebase.auth.GoogleAuthProvider());
+    var provider = new firebase.auth.GoogleAuthProvider();
+    let returnUrl = this.route.snapshot.queryParamMap.get('returnUrl') || '/';
+    localStorage.setItem('returnUrl', returnUrl);
+
+    this.afAuth.auth.signInWithPopup(provider).then(function (result) {
+      console.log(result);
+    };
   }
 
   logout() {
