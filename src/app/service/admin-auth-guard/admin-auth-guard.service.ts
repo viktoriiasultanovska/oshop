60da6655aba20f9f2c9ef2526824b5ab5d0b3807
Victoria Sultanovska <viktoriia.sultanovska@orba.co>
2018-05-20 21:23:44 +0200

    20.296. Project: Authentication and Authorization Protecting Admin Routes

--- src/app/service/admin-auth-guard/admin-auth-guard.service.ts
+++ src/app/service/admin-auth-guard/admin-auth-guard.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@angular/core';
-import {CanActivate, Router} from '@angular/router';
+import {CanActivate} from '@angular/router';
 import {AuthService} from '../auth/auth.service';
 import {UserService} from '../user/user.service';
 import 'rxjs/add/operator/switchMap';
@@ -7,13 +7,12 @@ import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/do';
 import 'rxjs/add/operator/take';
 import {Observable} from 'rxjs/Observable';
-import {AngularFireAuth} from 'angularfire2/auth';
 
 
 @Injectable()
 export class AdminAuthGuard implements CanActivate {
 
-  constructor(private auth: AuthService, private userService: UserService, private router: Router) {
+  constructor(private auth: AuthService, private userService: UserService) {
   }
 
 
