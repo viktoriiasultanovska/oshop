0f91a6392ef1434336299887e7d6531e3b9bda88
Victoria Sultanovska <viktoriia.sultanovska@orba.co>
2018-05-26 16:32:09 +0200

    21.305. Project: Product Management Module Adding a Bootstrap Preview Card

--- src/app/admin/product-form/product-form.component.ts
+++ src/app/admin/product-form/product-form.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {CategoryService} from '../../service/category/category.service';
 import {Observable} from 'rxjs/Observable';
 import {ProductService} from '../../service/product/product.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-product-form',
@@ -12,7 +13,11 @@ export class ProductFormComponent implements OnInit {
 
   categories: Observable<any[]>;
 
-  constructor(private categoryService: CategoryService, private productService: ProductService) {
+  constructor(
+    private categoryService: CategoryService,
+    private productService: ProductService,
+    private router: Router
+  ) {
     this.categories = this.categoryService.getCategories()
       .map(categories => {
         return categories.map(category => ({key: category.payload.key, ...category.payload.val()}));
@@ -23,6 +28,7 @@ export class ProductFormComponent implements OnInit {
 
   save(product) {
     this.productService.create(product);
+    this.router.navigate(['admin/products']);
   }
 
   ngOnInit() {
