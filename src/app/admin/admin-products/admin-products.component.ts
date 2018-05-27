f41f2f36dbcc74017a90632233de11a3b71bf1ec
Victoria Sultanovska <viktoriia.sultanovska@orba.co>
2018-05-27 23:50:00 +0200

    21.314. Project: Product Management Module Feeding the Data Table

--- src/app/admin/admin-products/admin-products.component.ts
+++ src/app/admin/admin-products/admin-products.component.ts
@@ -2,6 +2,7 @@ import {Component, OnDestroy, OnInit} from '@angular/core';
 import {ProductService} from '../../service/product/product.service';
 import {Subscription} from 'rxjs/Subscription';
 import {Product} from '../../models/product';
+import {DataTableResource} from 'angular5-data-table';
 
 @Component({
   selector: 'app-admin-products',
@@ -12,22 +13,49 @@ export class AdminProductsComponent implements OnInit, OnDestroy {
   products: Product[];
   filteredProducts: Product[];
   subscription: Subscription;
+  tableResource: DataTableResource<Product>;
+  items: Product[] = [];
+  itemCount: number;
 
   constructor(private productService: ProductService) {
     // Use snapshotChanges().map() to store the key
-    this.productService.getAll()
+    this.subscription = this.productService.getAll()
       .snapshotChanges()
       .map(changes => {
         return changes.map(c => ({key: c.payload.key, ...c.payload.val()}));
       })
       .subscribe(products => {
         this.filteredProducts = this.products = products;
+        this.initializeTable(products);
+      });
+  }
+
+  private initializeTable(products: Product[]) {
+    this.tableResource = new DataTableResource(products);
+    this.tableResource.query({offset: 0})
+      .then(items => {
+        this.items = items;
+        console.log(items);
+      });
+    this.tableResource.count()
+      .then(count => {
+        this.itemCount = count;
       });
   }
 
   filter(query: string) {
     this.filteredProducts = (query) ?
       this.products.filter(product => product.title.toLowerCase().includes(query.toLowerCase())) : this.products;
+  }
+
+  reloadItems(params) {
+    if (!this.tableResource) {
+      return;
+    }
+    this.tableResource.query(params)
+      .then(items => {
+        this.items = items;
+      });
   }
 
   ngOnInit() {
