14b354fb4e689321cd17404c7dbeda2adf034cbb
Victoria Sultanovska <viktoriia.sultanovska@orba.co>
2018-05-27 20:30:54 +0200

    21.311. Project: Product Management Module Extracting an Interface

--- src/app/admin/admin-products/admin-products.component.ts
+++ src/app/admin/admin-products/admin-products.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
 import {ProductService} from '../../service/product/product.service';
 import {Subscription} from 'rxjs/Subscription';
+import {Product} from '../../models/product';
 
 @Component({
   selector: 'app-admin-products',
@@ -8,8 +9,8 @@ import {Subscription} from 'rxjs/Subscription';
   styleUrls: ['./admin-products.component.css']
 })
 export class AdminProductsComponent implements OnInit, OnDestroy {
-  products: any[];
-  filteredProducts: any[];
+  products: Product[];
+  filteredProducts: Product[];
   subscription: Subscription;
 
   constructor(private productService: ProductService) {
